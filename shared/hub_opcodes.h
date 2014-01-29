d3aac83c7ada193f10c41a69e68d9f628dafd7ba
Bastien Clément <g@ledric.me>
2014-01-29 12:50:14 +0100

    Update opcodes

--- shared/hub_opcodes.h
+++ shared/hub_opcodes.h
@@ -13,8 +13,9 @@
 #define GET_SENSORS_2   0x62
 #define GET_SENSORS_3   0x63
 #define GET_SWITCHES    0x64
-#define SET_SWITCH_ON   0x65
-#define SET_SWITCH_OFF  0x66
+#define SET_SWITCHES    0x65
+#define SET_SWITCH_ON   0x66
+#define SET_SWITCH_OFF  0x67
 
 #define HELLO           0x5A
 #define RESET           0x7A
