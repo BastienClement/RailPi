2c82d7a23751cb395f1028e4c8b5e33afb0cbf2b
Bastien Clément <g@ledric.me>
2014-02-03 08:11:49 +0100

    Simple switch controller

--- raild/lua/main.lua
+++ raild/lua/main.lua
@@ -3,5 +3,12 @@ function OnReady()
 end
 
 function OnSensorChanged(sid, state)
-	SetSwitch(sid, state)
+	print("Sensor", sid, state)
+	if state and sid == 1 then
+		print("Switch", true)
+		SetSwitch(1, false)
+	elseif state and (sid == 9 or sid == 17) then
+		print("Switch", false)
+		SetSwitch(1, true)
+	end
 end
