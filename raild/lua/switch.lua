5c5e34a19d40483f0cf9038ab5c6a7c7dc2ebddf
galedric <g@ledric.me>
2014-05-06 21:00:45 +0200

    Most of switch handling code

--- raild/lua/switch.lua
+++ raild/lua/switch.lua
@@ -16,48 +16,59 @@ do
 end
 
 -- Creates a new auto-managed switch object
---      (1)  (2)
---      | | / /
---      | |/ /     STATE      EXIT
---      |/| /      false      (1)
---      | |/       true       (2)
+--
+--      (1) (2)
+--      | |/ /
+--      | / /     STATE      EXIT
+--      |/ /      false      (1)
+--      | /       true       (2)
 --      | |
 --      (3)
+--
 function CreateAutoSwitch(sid, sen1, sen2, sen3)
+    -- Check if this switch is already registered
+    if switches[sid] then
+        error("called CreateAutoSwitch with an already registered switchID")
+    end
+
     -- Bind event handler if not already done
     DoBind()
 
     -- Switch object
     local switch = {
         ["locked"] = false,
-        ["locked_source"] = -1,
-        ["locked_state"] = false,
+        ["exit_sensor"] = -1,
         ["state"] = false,
         ["handler"] = nil
     }
 
     -- Lock this switch in a given position
-    function switch:lock(state, source)
+    function switch:lock(state, exit)
+        self:set(state)
         self.locked = true
-        self.locked_state = state
-        self.locked_source = source
+        self.exit_sensor = exit
     end
 
     -- Free the previously set lock
-    function switch:unlock(state)
+    function switch:unlock()
         self.locked = false
-        self.locked_source = -1
-        if self.locked_state ~= self.state then
-            self:set(self.state)
-        end
+        self.exit_sensor = -1
     end
 
     -- Set the switch in a given position
     -- If the switch is locked, does nothing
-    function switch:set(state)
+    function switch:set(state, silent)
+        -- Ignore call if switch is already is this position
+        if self.state == state then return end
+
+        -- Update internal state
         self.state = state
+
+        -- If not locked, then send to RailHub
         if not self.locked then
             SetSwitch(sid, self.state)
+        else if not silent then
+            error("attempted to set a locked switch")
         end
     end
 
@@ -67,11 +78,42 @@ function CreateAutoSwitch(sid, sen1, sen2, sen3)
     end
 
     -- Handle sensor state changes
+    local delayTimer
     function switch:handleEvent(sen, state)
-        if self.locked then
-            -- Todo
-        else
-            -- Todo
+        -- Only react to related sensors
+        if sen == sen1
+        or sen == sen2
+        or sen == sen3 then
+            if self.locked and sen == self.exit_sensor then
+                -- Falling or rising edge on exit sensor
+                if not state then
+                    -- Falling edge, delay unlock by 500ms
+                    delayTimer = CreateTimer(500, 0, function()
+                        switch:unlock()
+                    end)
+                else
+                    -- Rising edge, cancel the unlock delay
+                    CancelTimer(delayTimer)
+                end
+            elseif state then
+                -- Rising edge on any sensor
+                if sen == sen1 then
+                    -- (1) -> (3)
+                    self:lock(false, 3)
+                elseif sen == sen2 then
+                    -- (2) -> (3)
+                    self:lock(true, 3)
+                else
+                    -- (3) -> (1|2)
+                    if self.handler then
+                        -- Call handler to set the switch
+                        self.handler()
+                    end
+
+                    -- Then lock it to the current state
+                    self:lock(self.state, self.state and sen1 or sen2)
+                end
+            end
         end
     end
 
