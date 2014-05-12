1977489afb65aeb00f2d3ce0b130913f4fcdedc9
Bastien Clément <g@ledric.me>
2014-05-12 08:48:09 +0200

    Change event name in main.lua too

--- raild/lua/main.lua
+++ raild/lua/main.lua
@@ -1,3 +1,13 @@
+local s1 = CreateSmartSwitch(1, 0, 0, 0)
+
+On("SensorChange", function(sen, state)
+    print("Sensor", sen, "to", state)
+    print("Set switch to", not GetSwitch(5))
+    SetSwitch(5, not GetSwitch(5))
+end)
+
+do return end
+
 local s1 = CreateSmartSwitch(1, 1,  9, 17)
 local s2 = CreateSmartSwitch(2, 2, 10, 18)
 local s3 = CreateSmartSwitch(3, 3, 11, 19)
@@ -6,9 +16,19 @@ local s5 = CreateSmartSwitch(5, 5, 13, 21)
 local s6 = CreateSmartSwitch(6, 6, 14, 22)
 local s7 = CreateSmartSwitch(7, 7, 15, 23)
 
--- Switch 1 toggles
-s1:setHandler(function()
-    self:toggle()
+function toggleHandler(self)
+    return function()
+        self:Toggle()
+    end
+end
+
+s1:SetHandler(toggleHandler(s1))
+s2:SetHandler(toggleHandler(s2))
+s4:SetHandler(toggleHandler(s4))
+s5:SetHandler(toggleHandler(s5))
+
+On("SensorChange", function(sen, state)
+    print(sen, state)
 end)
 
 On("Ready", function()
