bf75c8ada9049ab06ac9b5fefde61fb00815ada6
galedric <g@ledric.me>
2014-05-12 17:50:05 +0200

    Reverse switch behavior in RailHub

--- railhub/src/main.c
+++ railhub/src/main.c
@@ -29,42 +29,42 @@ void handle_input() reentrant {
 		case GET_SENSORS_1: SEND_DATA(SENSORS_1, ~P4); break;
 		case GET_SENSORS_2: SEND_DATA(SENSORS_2, ~P5); break;
 		case GET_SENSORS_3: SEND_DATA(SENSORS_3, ~P6); break;
-		
+
 		//
 		// Switches manipulation
 		//
-		case GET_SWITCHES: SEND_DATA(SWITCHES, P2); break;
-		
+		case GET_SWITCHES: SEND_DATA(SWITCHES, ~P2); break;
+
 		case SET_SWITCHES:
-			P2 = _getkey();
+			P2 = ~_getkey();
 			break;
-		
+
 		case SET_SWITCH_ON:
-			P2 |= (1 << _getkey());
-			break;
-		
-		case SET_SWITCH_OFF:
 			P2 &= ~(1 << _getkey());
 			break;
-	
+
+		case SET_SWITCH_OFF:
+			P2 |= (1 << _getkey());
+			break;
+
 		//
 		// Power control
 		//
 		case POWER_ON:
 			led = 1;
 			break;
-		
+
 		case POWER_OFF:
 			led = 0;
 			break;
-		
+
 		//
 		// Keep-Alive
 		//
 		case KEEP_ALIVE:
 			keepalive_timeout = 0;
 			break;
-		
+
 		//
 		// Reset
 		//
@@ -75,7 +75,7 @@ void handle_input() reentrant {
 
 void dead() {
 	led = 0;
-	
+
 	while(1) {
 		WATCHDOG;
 		if(RI0 == 1) {
@@ -86,17 +86,17 @@ void dead() {
 
 void watch_sensors() {
 	uint8 _shadow;
-	uint8 switches = P2;
+	uint8 switches = ~P2;
 	uint8 sensors1 = ~P4;
 	uint8 sensors2 = ~P5;
 	uint8 sensors3 = ~P6;
-	
+
 	SEND_DATA(SENSORS_1, ~P4);
 	SEND_DATA(SENSORS_2, ~P5);
 	SEND_DATA(SENSORS_3, ~P6);
-	
+
 	putchar_raw(READY);
-	
+
 	#define DO_SHADOW(shadow, port, opcode, action) \
 		if(_shadow = (port), _shadow != shadow) { \
 			shadow = _shadow; \
@@ -104,19 +104,19 @@ void watch_sensors() {
 			WATCHDOG; \
 			SEND_DATA(opcode, shadow); \
 		}
-		
+
 	while(1) {
 		WATCHDOG;
-		
-		DO_SHADOW(switches, P2, SWITCHES,);
+
+		DO_SHADOW(switches, ~P2, SWITCHES,);
 		DO_SHADOW(sensors1, ~P4, SENSORS_1,);
 		DO_SHADOW(sensors2, ~P5, SENSORS_2,);
 		DO_SHADOW(sensors3, ~P6, SENSORS_3,);
-		
+
 		if(RI0 == 1) {
 			handle_input();
 		}
-		
+
 		if(--keepalive == 0) {
 			if(++keepalive_timeout > 2) {
 				dead();
@@ -134,14 +134,14 @@ void watch_sensors() {
 void main(void) {
 	WDTCN = 0xFF;
 	WATCHDOG;
-	
+
 	SYSCLK_Init(); // initialize oscillator
 	PORT_Init();   // initialize crossbar and GPIO
 	UART0_Init();  // initialize UART0
-	
+
 	led = 1;
-	P2  = 0x00;
-	
+	P2  = 0xFF;
+
 	putchar_raw(HELLO);
 	watch_sensors();
 }
