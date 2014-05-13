4a21f7391240a614bcdada471b8c94d657ef4230
Bastien Clément <g@ledric.me>
2014-05-13 09:32:54 +0200

    Update main.lua

--- raild/lua/main.lua
+++ raild/lua/main.lua
@@ -1,36 +1,31 @@
-local s1 = CreateSwitch(1, 0, 0, 0)
+local s1 = Switch:Create(1, 1,  9, 17)
+local s2 = Switch:Create(2, 2, 10, 18)
+local s3 = Switch:Create(3, 3, 11, 19)
+local s4 = Switch:Create(4, 4, 12, 20)
+local s5 = Switch:Create(5, 5,  4, 21)
 
-On("SensorChange", function(sen, state)
-    print("Sensor", sen, "to", state)
-    print("Set switch to", not GetSwitch(5))
-    SetSwitch(5, not GetSwitch(5))
-end)
-
-do return end
-
-local s1 = CreateSwitch(1, 1,  9, 17)
-local s2 = CreateSwitch(2, 2, 10, 18)
-local s3 = CreateSwitch(3, 3, 11, 19)
-local s4 = CreateSwitch(4, 4, 12, 20)
-local s5 = CreateSwitch(5, 5, 13, 21)
-local s6 = CreateSwitch(6, 6, 14, 22)
-local s7 = CreateSwitch(7, 7, 15, 23)
-
-function toggleHandler(self)
+function toggleHandler(switch)
     return function()
-        self:Toggle()
+        switch:Toggle()
     end
 end
 
-s1:SetHandler(toggleHandler(s1))
-s2:SetHandler(toggleHandler(s2))
-s4:SetHandler(toggleHandler(s4))
-s5:SetHandler(toggleHandler(s5))
+s1:On("EnterC", toggleHandler(s1))
+s2:On("EnterC", toggleHandler(s2))
+s4:On("EnterC", toggleHandler(s4))
+s5:On("EnterC", toggleHandler(s5))
 
-On("SensorChange", function(sen, state)
-    print(sen, state)
+s3:On("EnterC", function()
+    s3:Set(false)
 end)
 
 On("Ready", function()
     SetPower(true)
+end)
+
+local interest = { [6] = true, [7] = true, [8] = true, [14] = true, [15] = true, [16] = true, [22] = true, [23] = true, [23] = true,  }
+
+On("SensorChange", function(sen, state)
+    --if interest[sen] then return end
+    print(sen, state)
 end)
