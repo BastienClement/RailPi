c7d2cc7323e1149f0bd4516843c282455227bf8e
galedric <g@ledric.me>
2014-05-15 17:55:41 +0200

    Lot of changes for a poor main.lua

--- raild/lua/main.lua
+++ raild/lua/main.lua
@@ -1,24 +1,23 @@
-local s1 = Switch:Create(1, 1,  9, 17)
-local s2 = Switch:Create(2, 2, 10, 18)
-local s3 = Switch:Create(3, 3, 11, 19)
-local s4 = Switch:Create(4, 4, 12, 20)
-local s5 = Switch:Create(5, 5,  4, 21)
+Sensors.Disable({ 8, 16, 24, 13, 23 })
 
-Switch:On("EnterC", function(switch)
-    if switch.id == 3 then
-        switch:Set(false)
+Switches[1].Enable(1,  9, 17)
+Switches[2].Enable(2, 10, 18)
+Switches[3].Enable(3, 11, 19)
+Switches[4].Enable(4, 12, 20)
+Switches[5].Enable(5,  4, 21)
+
+Switches.On("EnterC", function(switch)
+    if switch.GetId() == 3 then
+        switch.SetState(false)
     else
-        switch:Toggle()
+        switch.SetState(not switch.GetState())
     end
+end)
+
+Sensors.On("Change", function(sensor, state)
+    print(sensor.GetId(), state)
 end)
 
 On("Ready", function()
     SetPower(true)
-end)
-
-local unconnected = { [8] = true, [16] = true, [24] = true, [13] = true, [23] = true }
-
-On("SensorChange", function(sen, state)
-    --if unconnected[sen] then return end
-    print(sen, state)
 end)
