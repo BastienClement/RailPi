9af371d0020d2017d94c4d6c01574ca874156135
galedric <g@ledric.me>
2014-05-15 17:26:57 +0200

    Rewrite switch.lua to match sensor.lua

--- raild/lua/switch.lua
+++ raild/lua/switch.lua
@@ -2,24 +2,13 @@
 -- Auto-managed switches
 --
 Switch = EventEmitter({
-    handlers = {}
+    -- Delay before unlocking the switch (ms)
+    debounce = 100
 })
 
--- Bind the event handler on first Swtich:Create() call
-local DoBind
-do
-    local bound = false
-    function DoBind()
-        if bound then return else bound = true end
-        On("SensorChange", function(sid, state)
-            for _, handler in pairs(Switch.handlers) do
-                handler(sid, state)
-            end
-        end)
-    end
-end
+local switch_mt = { __tostring = function() return "[object Switch]" end }
 
--- Creates a new auto-managed switch object
+-- Lazy sensor creation
 --
 --      (A) (B)
 --      | |/ /
@@ -29,93 +18,60 @@ end
 --      | |
 --      (C)
 --
-function Switch:Create(sid, senA, senB, senC)
-    local handlers = self.handlers
-
-    -- Check if this switch is already registered
-    if handlers[sid] then
-        error("called Switch:Create with an already registered switchID")
-    end
-
-    -- Bind event handler if not already done
-    DoBind()
-
-    -- Switch object
-    local this = EventEmitter({
-        ["id"] = sid,
-        ["locked"] = false,
-        ["exit_sensor"] = -1,
-        ["enter_sensor"] = -1,
-        ["state"] = false
-    })
-
-    -- Emit events on both the switch object and the global
-    local function emit(event, ...)
-        this:Emit(event, ...)
-        Switch:Emit(event, this, ...)
-    end
-
-    -- Lock this switch in a given position
-    function this:Lock(state)
-        self:Set(state)
-        self.locked = true
-        emit("Lock", state)
-    end
-
-    -- Free the previously set lock
-    function this:Unlock()
-        self.locked = false
-        self.enter_sensor = -1
-        self.exit_sensor = -1
-        emit("Unlock")
-    end
-
-    -- Set the switch in a given position
-    -- If the switch is locked, does nothing
-    function this:Set(state, silent)
-        -- Ignore call if switch is already is this position
-        if self.state == state then return end
-
-        -- Update internal state
-        self.state = state
-
-        -- If not locked, then send to RailHub
-        if not self.locked then
-            SetSwitch(sid, self.state)
-            emit("Set", state)
-        elseif not silent then
-            error("attempt to set or lock an already locked switch")
+setmetatable(Switch, {
+    __index = function(_, id)
+        if type(id) ~= "number"
+        or id < 1 or id > 8 then
+            error("invalid switch id: " .. id)
         end
-    end
 
-    -- Toggle switch state
-    function this:Toggle(silent)
-        self:Set(not self.state, silent)
-    end
+        -- The switch object
+        local self = EventEmitter()
+        local enabled = false
+        local state = false
+        local pending_state = false
+        local locked = false
 
-    -- Register this switch and its event handler
-    local delayTimer
-    handlers[sid] = function(sen, state)
-        -- Only react to related sensors
-        if sen == senA
-        or sen == senB
-        or sen == senC then
-            if this.locked then
-                if sen == this.exit_sensor then
-                    -- Falling or rising edge on exit sensor
-                    if not state then
-                        -- Falling edge, delay unlock by 100ms
-                        delayTimer = CreateTimer(100, 0, function()
-                            this:Unlock()
-                        end)
+        local senA, senB, senC
+        local enter_sensor, exit_sensor
+        local delay
+
+        -- Emit event on both the switch itself and the global Switch object
+        local function emit(event, ...)
+            self.Emit(event, ...)
+            Sensor.Emit(event, self, ...)
+        end
+
+        -- Handle sensor events
+        local function handler(sen, rising)
+            -- Only react to related sensors
+            if sen == senA
+            or sen == senB
+            or sen == senC then
+                if locked then
+                    if sen == exit_sensor then
+                        -- Falling or rising edge on exit sensor
+                        if rising then
+                            -- Rising edge, cancel the unlock delay
+                            if delay then
+                                CancelTimer(delay)
+                                delay = nil
+                            end
+                        else
+                            -- Falling edge, delay unlock
+                            delay = CreateTimer(Switch.debounce, 0, function()
+                                locked = false
+                                enter_sensor = nil
+                                exit_sensor = nil
+                                emit("Unlock")
+                                self.Set(pending_state)
+                            end)
+                        end
                     else
-                        -- Rising edge, cancel the unlock delay
-                        CancelTimer(delayTimer)
-                    end
-                else
-                    if state then
-                        if sen ~= this.enter_sensor
-                        and sen ~= this.exit_sensor then
+                        -- Activity on another sensor
+                        if  rising
+                        and sen ~= enter_sensor
+                        and sen ~= exit_sensor then
                             -- The current train has nothing to do with this
                             -- sensor, power-offing
                             SetPower(false)
@@ -123,42 +79,92 @@ function Switch:Create(sid, senA, senB, senC)
                             emit("BadSensor", sen)
                         end
                     end
-                end
-            elseif state then
-                -- Rising edge on any sensor
-                if sen == senA then
-                    -- (A) -> (C)
-                    this.enter_sensor = senA
-                    this.exit_sensor = senC
-                    this:Lock(false, senC)
-                    emit("EnterA")
-                elseif sen == senB then
-                    -- (B) -> (C)
-                    this.enter_sensor = senB
-                    this.exit_sensor = senC
-                    this:Lock(true, senC)
-                    emit("EnterB")
-                else
-                    -- (C) -> (A|B)
-                    emit("EnterC")
-                    this.enter_sensor = senC
-                    this.exit_sensor = this.state and senB or senA
+                elseif rising then
+                    -- Rising edge,  lock this switch
+                    if sen == senA then
+                        -- (A) -> (C)
+                        emit("EnterA")
+                        enter_sensor = senA
+                        exit_sensor = senC
+                        self.Set(false)
+                    elseif sen == senB then
+                        -- (B) -> (C)
+                        emit("EnterB")
+                        enter_sensor = senB
+                        exit_sensor = senC
+                        self.Set(true)
+                    else
+                        -- (C) -> (A|B)
+                        emit("EnterC")
+                        enter_sensor = senC
+                        exit_sensor = state and senB or senA
+                    end
 
-                    -- Lock it to the current state
-                    this:Lock(this.state)
+                    locked = true
+                    emit("Lock")
                 end
             end
         end
-    end
 
-    -- Destroys a SmartSwitch
-    -- The object should not be used afterward
-    function this:Disable()
-        emit("Disable", sid, state, exit)
-        handlers[sid] = nil
-    end
+        -- Enable this switch and bind it to given sensors
+        function self.Enable(a, b, c)
+            if enabled then
+                error("attempt to enable an already enabled switch")
+            end
 
-    -- Return the new switch object
-    self[sid] = this
-    return this
-end
+            -- Cast sensor IDs to sensor objects
+            if type(a) == "number" then a = Sensor[a] end
+            if type(b) == "number" then b = Sensor[b] end
+            if type(c) == "number" then b = Sensor[b] end
+
+            -- Check if arguments are valid sensor objects
+            if not Sensor.IsSensor(a)
+            or not Sensor.IsSensor(b)
+            or not Sensor.IsSensor(c) then
+                error("invalid arguments given to switch.Enable()")
+            end
+
+            -- Save them
+            senA, senB, senC = a, b, c
+
+            -- Sync switch state
+            enabled = true
+            state = GetSwitch(id)
+            pending_state = state
+            locked = false
+
+            -- Register handler
+            Sensor:On("Change", handler)
+
+            emit("Enable")
+            return self
+        end
+
+        -- Disable this switch
+        function self.Disable()
+            Sensor:Off("Change", handler)
+            enabled = false
+            emit("Disable")
+            return self
+        end
+
+        -- Set the switch in a given position
+        function self.Set(new_state)
+            if locked then
+                -- Store the state for later
+                pending_state = new_state
+            elseif state ~= new_state then
+                -- Update state
+                state = new_state
+                pending_state = new_state
+                SetSwitch(id, new_state)
+                emit("Set", new_state)
+            end
+            return self
+        end
+
+        -- Register and this switch
+        Switch[id] = self
+        return setmetatable(self, switch_mt)
+    end
+})
