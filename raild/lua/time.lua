5980de845771f3d1098ce3dce06e3fa3ad056006
Bastien Clément <g@ledric.me>
2014-05-19 09:24:35 +0200

    Add chrono helper

--- raild/lua/time.lua
+++ raild/lua/time.lua
@@ -1,3 +1,6 @@
+--
+-- Get current time
+--
 local ffi = require("ffi")
 ffi.cdef[[
     typedef long time_t;
@@ -15,4 +18,27 @@ local now = ffi.new("struct timeval")
 function Time()
     C.gettimeofday(now, nil)
     return now.tv_sec + (now.tv_usec / 1000000)
+end
+
+--
+-- Chrono helper
+--
+function Chrono()
+    local self = {}
+    local start = Time()
+
+    -- Reset this chrono start time
+    function self.Reset()
+        start = Time()
+        return self
+    end
+
+    -- Get current time relative to the chrono start time
+    function self.Time(reset)
+        local t = Time() - start
+        if reset then self.Start() end
+        return t
+    end
+
+    return self
 end
